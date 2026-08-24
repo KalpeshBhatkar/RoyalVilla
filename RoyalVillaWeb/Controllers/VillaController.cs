152d47fd2ba620be13431c93ea3be5573d98582c
Kalpesh Bhatkar <kalpeshbhatkar916@gmail.com>
2026-08-24 20:19:13 +0530

    Edit Villa implemented using API

--- RoyalVillaWeb/Controllers/VillaController.cs
+++ RoyalVillaWeb/Controllers/VillaController.cs
@@ -103,5 +103,47 @@ namespace RoyalVillaWeb.Controllers
             }
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["error"] = $"Invalid villa ID";
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+                var response = await _villaService.GetAsync<ApiResponse<VillaDTO>>(id, "");
+                if (response != null && response.Success && response.Data != null)
+                {
+                    View(_mapper.Map<UpdateVillaDTO>(response.Data));
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = $"An error occurred: {ex.Message}";
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(UpdateVillaDTO villaUpdateDTO)
+        {
+            try
+            {
+                var response = await _villaService.UpdateAsync<ApiResponse<object>>(villaUpdateDTO, "");
+                if (response != null && response.Success && response.Data != null)
+                {
+                    TempData["success"] = "Villa updated successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = $"An error occurred: {ex.Message}";
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
