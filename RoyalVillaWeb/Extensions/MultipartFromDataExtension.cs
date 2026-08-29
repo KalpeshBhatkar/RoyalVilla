89474e01c49c07689f0abe9b42ffe2d7b17caef7
Kalpesh Bhatkar <kalpeshbhatkar916@gmail.com>
2026-08-29 11:04:05 +0530

    Added Extension for Multipart Form Data to handle image and string content

---
+++ RoyalVillaWeb/Extensions/MultipartFromDataExtension.cs
@@ -0,0 +1,36 @@
+﻿namespace RoyalVillaWeb.Extensions
+{
+    public static class MultipartFromDataExtension
+    {
+        public static MultipartFormDataContent ToMultipartFormData(this object obj)
+        {
+            var formData = new MultipartFormDataContent();
+            var proparties = obj.GetType().GetProperties();
+
+            foreach (var property in proparties)
+            {
+                var value = property.GetValue(obj);
+
+                if (value != null)
+                {
+                    continue;
+                }
+
+                var propartyName = property.Name;
+
+                if (value is IFormFile file && file.Length > 0)
+                {
+                    var streamContent = new StreamContent(file.OpenReadStream());
+                    streamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(file.ContentType);
+                    formData.Add(streamContent, propartyName, file.FileName);
+                }
+                else if (value is not IFormFile)
+                {
+                    formData.Add(new StringContent(value.ToString()!), propartyName);
+                }
+            }
+
+            return formData;
+        }
+    }
+}
